a7e8644c18aafbab59cf253a62d3bb32a9de45d9
diegoProFP <diego.di1@educa.madrid.org>
2024-02-19 18:43:20 +0100

    Initial commit

---
+++ src/polonorte/PruebaNavidad.java
@@ -0,0 +1,24 @@
+package polonorte;
+
+import java.time.LocalDate;
+
+import com.polonorte.elfos.Diego;
+import com.polonorte.elfos.ElfoGenerico;
+
+public class PruebaNavidad {
+
+	public static void main(String[] args) throws DatosInvalidosException {
+
+		SantaClaus santa = new SantaClaus();
+		String caracterAleatorio = Utils.generarCaracterAleatorio();
+		
+		LocalDate fechaNavidad = Utils.generarDiaAleatorioDiciembre();
+		boolean santaTrabajando = santa.comprobarTrabajo(fechaNavidad);
+		
+		System.out.println("La fecha de hoy es: " + fechaNavidad);
+		System.out.println("¿Está santa trabajando?: " + Utils.convertirBooleanASiNo(santaTrabajando));
+		System.out.println("El carácter de santa es: " + santa.generarSaludoSegunCaracter(caracterAleatorio));
+		
+	}
+
+}
